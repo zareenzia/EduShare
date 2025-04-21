16770748801e8bc740799a28071d830caa527375
Zareen Zia <zareenzia801@gmail.com>
2025-04-21 21:16:47 +0600

    initial commit by zareen

---
+++ src/main/java/com/edushare/file_sharing_app_backend/FileSharingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edushare.file_sharing_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSharingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSharingAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edushare/file_sharing_app_backend/FileSharingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edushare.file_sharing_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSharingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
